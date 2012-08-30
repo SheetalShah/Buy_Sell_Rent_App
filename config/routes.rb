37c58c1aa1b63bc5e219a742532e1fc6cf568b0a
Sheetal <onlysheetal@gmail.com>
2012-08-30 11:34:18 +0530

    After chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -41,9 +41,8 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
+  map.connect '/BuySellRentApp/', :controller => 'BuySellRentApp', :action => 'home'
   map.connect '/BuySellRentApp/home', :controller => 'BuySellRentApp', :action => 'home'
   map.connect '/BuySellRentApp/help', :controller => 'BuySellRentApp', :action => 'help'
   map.connect '/BuySellRentApp/about', :controller => 'BuySellRentApp', :action => 'about'
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
 end
